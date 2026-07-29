cfc7809d8f28b5a20d413f9f135890a6fe1ccaac
TIGTM <TIGTM@users.noreply.github.com>
2026-07-29 15:30:26 -0300

    chore: fixa porta interna de producao

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -10,6 +10,8 @@ module.exports = {
             max_memory_restart: '512M',
             env: {
                 NODE_ENV: 'production',
+                HOST: '127.0.0.1',
+                PORT: 3025,
             },
         },
     ],
